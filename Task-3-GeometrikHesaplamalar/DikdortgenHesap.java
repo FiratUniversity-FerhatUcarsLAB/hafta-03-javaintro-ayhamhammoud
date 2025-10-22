cd5ef00572527c880dfa0ad5dd56bd564f090322
ayhamhammoud <240541613@firat.edu.tr>
2025-10-22 19:10:36 +0300

    Update DikdortgenHesap.class

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,17 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        // Genişlik (en) ve yükseklik (boy)
+        double en = 4.5;
+        double boy = 7.9;
+
+        // Alan ve çevre hesapla
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        // Sonuçları yazdır
+        System.out.println("En    : " + en);
+        System.out.println("Boy   : " + boy);
+        System.out.println("Alan  : " + alan);
+        System.out.println("Cevre : " + cevre);
+    }
+}
