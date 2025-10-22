56a75598363013318a14b03fe2fde5f790f12cd0
ayhamhammoud <240541613@firat.edu.tr>
2025-10-22 20:10:58 +0300

    Update and rename MilKilometreTablosu.java to MilKilometreTablosuClass.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosuClass.java
@@ -0,0 +1,16 @@
+public class DonusumTablosu {
+    public static void main(String[] args) {
+        double katsayi = 1.609;
+
+        System.out.println("Mil\t Kilometre");
+        System.out.println("-------------------");
+
+        int[] milDegerleri = {1, 5, 10, 20, 50};
+
+        for (int i = 0; i < milDegerleri.length; i++) {
+            int mil = milDegerleri[i];
+            double km = mil * katsayi;
+            System.out.println(mil + "\t " + km);
+        }
+    }
+}
